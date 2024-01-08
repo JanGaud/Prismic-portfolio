e6f2ce91f490c39a28146cd6b230d2fbd4f0bad1
JanGaud <JanisGaudreault.jg@gmail.com>
2024-01-08 12:59:39 -0500

    Slice almost done

--- src/lib/slices/About/index.svelte
+++ src/lib/slices/About/index.svelte
@@ -8,7 +8,7 @@
 </script>
 
 <section
-	class="my-20 h-screen justify-items-center grid-cols-1 grid lg:grid-cols-2"
+	class="my-20 h-screen justify-items-center grid-cols-1 grid lg:grid-cols-2 xl:gap-20"
 	data-slice-type={slice.slice_type}
 	data-slice-variation={slice.variation}
 >
@@ -18,13 +18,13 @@
 			style="background: rgb(255,255,255);
 				background: radial-gradient(circle, rgba(255,255,255,0) 42%, rgba(0,0,0,0.87718837535014) 64%, rgba(0,0,0,1) 73%);"
 		></div>
-		<img class="object-cover h-full w-auto" src={image} alt="" />
+		<img class="object-contain h-full" src={image} alt="" />
 	</div>
 
 	<div class="text-fourth text-center flex flex-col justify-center gap-3">
-		<hgroup>
-			<h2 class="text-6xl"><PrismicRichText field={slice.primary.about_title} /></h2>
-			<h3 class="text-lg"><PrismicRichText field={slice.primary.about_subtitle} /></h3>
+		<hgroup class="z-30">
+			<h2 class="neonText text-3xl md:text-6xl"><PrismicRichText field={slice.primary.about_title} /></h2>
+			<h3 class="md:text-lg"><PrismicRichText field={slice.primary.about_subtitle} /></h3>
 		</hgroup>
 		<div class="flex flex-col gap-3">
 			{#each slice.items as item}
@@ -35,3 +35,18 @@
 		</div>
 	</div>
 </section>
+
+<style>
+	.neonText {
+		color: #fff;
+		text-shadow:
+			0 0 7px #fff,
+			0 0 10px #fff,
+			0 0 21px #fff,
+			0 0 42px #F82124,
+			0 0 82px #F82124,
+			0 0 92px #F82124,
+			0 0 102px #F82124,
+			0 0 151px #F82124;
+	}
+</style>
