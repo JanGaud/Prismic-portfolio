61cbbf6f06cf1573300f2160dc26db557f8d0004
JanGaud <JanisGaudreault.jg@gmail.com>
2024-01-10 17:07:26 -0500

    Finished for the day

--- src/lib/components/SocialNav.svelte
+++ src/lib/components/SocialNav.svelte
@@ -25,9 +25,9 @@
 
 {#if social_active}
     <div class="flex flex-col gap-5 fixed top-1/2 -translate-y-1/2 right-0 z-40">
-        <ul class="backdrop-blur-md rounded-2xl">
+        <ul class="rounded-2xl">
             {#each social.filter((item) => item && item.social_media_link) as item}
-                <li class="w-[300px] backdrop-blur-sm rounded-2xl px-2 h-14 flex justify-between items-center mr-[-254px] hover:ml-[-210px] duration-500">
+                <li class="w-[300px] backdrop-blur-lg rounded-3xl px-2 h-14 flex justify-between items-center mr-[-254px] hover:ml-[-210px] duration-500">
                     <PrismicLink field={item.social_media_link} class="flex justify-between items-center w-full">
                         <div class="flex items-center gap-3">
                             <Icon
