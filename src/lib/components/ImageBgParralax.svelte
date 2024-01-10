cb92bd77ac775ff166653529ac11e2b4bc866958
JanGaud <JanisGaudreault.jg@gmail.com>
2024-01-10 13:35:22 -0500

    Parralax adjustement

--- src/lib/components/ImageBgParralax.svelte
+++ src/lib/components/ImageBgParralax.svelte
@@ -12,7 +12,7 @@
 		<div
 			class="w-full h-40 absolute top-0 z-20"
 			style="background: rgb(255,255,255);
-background: linear-gradient(0deg, rgba(255,255,255,0) 7%, rgba(0,0,0,0.3925945378151261) 29%, rgba(0,0,0,0.7035189075630253) 55%, rgba(0,0,0,1) 93%)"
+background: linear-gradient(0deg, rgba(255,255,255,0) 0%, rgba(0,0,0,0.3925945378151261) 11%, rgba(0,0,0,0.7035189075630253) 21%, rgba(0,0,0,1) 34%);"
 		></div>
 
 		<slot />
